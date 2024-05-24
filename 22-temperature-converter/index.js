71468a4aab9f270bf4ea6ea938b5b9b3004eede5
aazzam <amandas.azzam@gmail.com>
2024-05-23 20:16:03 -0800

    Update project 22: temperature converter

---
+++ 22-temperature-converter/index.js
@@ -0,0 +1,26 @@
+const celciusEl = document.getElementById("celcius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+
+function computeTemp(event) {
+  const currentValue = +event.target.value;
+
+  switch (event.target.name) {
+    case "celcius":
+      kelvinEl.value = (currentValue + 273.32).toFixed(2);
+      fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+
+      break;
+    case "fahrenheit":
+      celciusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+
+      break;
+    case "kelvin":
+      celciusEl.value = (currentValue - 273.32).toFixed(2);
+      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+      break;
+    default:
+      break;
+  }
+}
